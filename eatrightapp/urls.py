0e3919d3e574f8f3eb91ad91b130d7151933e307
peayah <82358003+peayah@users.noreply.github.com>
2023-02-19 22:08:54 -0800

    delete the '' url set up

--- eatrightapp/urls.py
+++ eatrightapp/urls.py
@@ -1,14 +1,12 @@
 from django.contrib import admin
+from django.urls import path
 from django.urls import include
 from django.views.generic import RedirectView
 from django.conf import settings
 from django.conf.urls.static import static
-import catalog.views
-from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', catalog.views.index, name="index"),
 
     path('catalog/', include('catalog.urls')),
     path('', RedirectView.as_view(url='catalog/', permanent=True)),
