5565ad95eb2a546117f3fe376e07705af649bd79
peayah <82358003+peayah@users.noreply.github.com>
2023-02-20 01:29:05 -0800

    move urls

--- eatrightapp/urls.py
+++ eatrightapp/urls.py
@@ -11,16 +11,12 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('url/', include('catalog.urls')),
+    # path('catalog/', include('catalog.urls')),
     path('', RedirectView.as_view(url='catalog/', permanent=True)),
+    path('accounts/', include('django.contrib.auth.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
-urlpatterns += [
-    path('catalog/', include('catalog.urls')),
-]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-urlpatterns += [
-    path('accounts/', include('django.contrib.auth.urls')),
-]
 
