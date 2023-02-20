17c1c6ec92de2e5d18f4b8eb9585fbf9e1c52e43
peayah <82358003+peayah@users.noreply.github.com>
2023-02-19 22:38:12 -0800

    serve catalog.urls

--- eatrightapp/urls.py
+++ eatrightapp/urls.py
@@ -11,10 +11,12 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('catalog/', include('catalog.urls')),
-    path('accounts/', include('django.contrib.auth.urls')),
     path('', RedirectView.as_view(url='catalog/', permanent=True)),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+urlpatterns += [
+    path('catalog/', include('catalog.urls')),
+]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
