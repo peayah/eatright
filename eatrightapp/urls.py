13e09b21f7fed748274c34688ba7ae7da19a351e
peayah <82358003+peayah@users.noreply.github.com>
2023-02-19 22:25:49 -0800

    add new urls

--- eatrightapp/urls.py
+++ eatrightapp/urls.py
@@ -1,14 +1,23 @@
+# cohorts URL Configuration
+
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
 from django.views.generic import RedirectView
+# Use static() to add URL mapping to serve static
+# files during development (only)
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
     path('catalog/', include('catalog.urls')),
-    path('', RedirectView.as_view(url='catalog/', permanent=True)),
     path('accounts/', include('django.contrib.auth.urls')),
-    ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    path('', RedirectView.as_view(url='catalog/', permanent=True)),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += [
+    path('accounts/', include('django.contrib.auth.urls')),
+]
