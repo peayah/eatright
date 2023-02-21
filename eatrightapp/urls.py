a1c61fc75c7ca46b4df4ed7484b879507968b12b
peayah <82358003+peayah@users.noreply.github.com>
2023-02-20 22:27:49 -0800

    catalog links

--- eatrightapp/urls.py
+++ eatrightapp/urls.py
@@ -3,14 +3,11 @@ from django.urls import path, include
 from django.views.generic import RedirectView
 from django.conf import settings
 from django.conf.urls.static import static
-import catalog
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('catalog.tests')),
-    path('', include('catalog.views')),
-    path('', include('catalog.urls')),
+
+    path('catalog/', include('catalog.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('', RedirectView.as_view(url='catalog/', permanent=True)),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
