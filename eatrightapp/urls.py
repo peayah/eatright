cda176fa1c294659bd895577c785e61fed56ed9d
peayah <82358003+peayah@users.noreply.github.com>
2023-02-20 18:59:10 -0800

    try new path

--- eatrightapp/urls.py
+++ eatrightapp/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
     path('', RedirectView.as_view(url='catalog/', permanent=True)),
-    # path('', include("catalog.urls")),
+    path('catalog', "urls"),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
