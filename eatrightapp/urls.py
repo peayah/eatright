d2effeba18dd709cd5973a96530ddb4e3551cf51
peayah <82358003+peayah@users.noreply.github.com>
2023-02-20 17:06:25 -0800

    comment out user auth

--- eatrightapp/urls.py
+++ eatrightapp/urls.py
@@ -1,10 +1,6 @@
-# cohorts URL Configuration
-
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
-# # Use static() to add URL mapping to serve static
-# # files during development (only)
 from django.conf import settings
 from django.conf.urls.static import static
 
