7ba9f10031ef26ccf5d3aff691525ce780f8414a
peayah <82358003+peayah@users.noreply.github.com>
2023-02-20 20:20:33 -0800

    delete catalog

--- eatrightapp/urls.py
+++ eatrightapp/urls.py
@@ -3,12 +3,26 @@ from django.urls import path, include
 from django.views.generic import RedirectView
 from django.conf import settings
 from django.conf.urls.static import static
+import catalog
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('catalog/', include('catalog.urls')),
+    path('', include('catalog.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('', RedirectView.as_view(url='catalog/', permanent=True)),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+# <code>
+# path('folio/', include('folio.urls'))
+# </code>
+#
+# to be this
+#
+# <code>
+# path('', include('folio.urls'))
+#
+# </code>
