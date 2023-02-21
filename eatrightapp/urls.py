415da84fdf855fb72e6f369365149304b77391a6
peayah <82358003+peayah@users.noreply.github.com>
2023-02-20 19:34:30 -0800

    add new url code

--- eatrightapp/urls.py
+++ eatrightapp/urls.py
@@ -6,9 +6,9 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('catalog/', include('catalog.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('', RedirectView.as_view(url='catalog/', permanent=True)),
-    path('catalog', include('catalog.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
