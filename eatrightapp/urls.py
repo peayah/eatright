1272f56df22b77ca9b5894f05da93b5fb5a580a9
peayah <82358003+peayah@users.noreply.github.com>
2023-02-22 00:58:35 -0800

    change name to extra_patterns

--- eatrightapp/urls.py
+++ eatrightapp/urls.py
@@ -1,13 +1,13 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
 from django.views.generic import RedirectView
 from django.conf import settings
 from django.conf.urls.static import static
+from catalog.urls import extra_patterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('/', include('catalog.urls')),
-    path('catalog/', include('catalog.urls')),
+    path('catalog/', include(extra_patterns)),
     path('accounts/', include('django.contrib.auth.urls')),
     path('', RedirectView.as_view(url='catalog/', permanent=True)),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
