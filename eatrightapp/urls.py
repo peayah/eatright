5c2d8867ac0339bf4964dc85758568f306a62344
peayah <82358003+peayah@users.noreply.github.com>
2023-02-22 01:05:05 -0800

    change name back

--- eatrightapp/urls.py
+++ eatrightapp/urls.py
@@ -3,14 +3,21 @@ from django.urls import include, path
 from django.views.generic import RedirectView
 from django.conf import settings
 from django.conf.urls.static import static
-from catalog.urls import extra_patterns
+from catalog.urls import urlpatterns
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('catalog/', include(extra_patterns)),
-    path('accounts/', include('django.contrib.auth.urls')),
-    path('', RedirectView.as_view(url='catalog/', permanent=True)),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    path('admin/',
+         admin.site.urls),
+    path('catalog/',
+         include(urlpatterns)),
+    path('accounts/',
+         include('django.contrib.auth.urls')),
+    path('',
+         RedirectView.as_view(url='catalog/',
+                                  permanent=True)),
+] + static(settings.STATIC_URL,
+           document_root=settings.STATIC_ROOT)
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL,
+                      document_root=settings.MEDIA_ROOT)
 
